f3916c638675c8da0a55dbda6d53907eb77beb71
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-08-05 15:21:05 +0200

    create table user and add fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        
+    ){}
+
+    public function load(ObjectManager $manager): void
+    {
+       $admin = new User();
+        $admin->setEmail('cyril.gourdon.02@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'admin')
+        );
+        $admin->setName('BodyPlanet');
+        $admin->setAddress('2, allée des Anges');
+        $admin->setZipcode('02200');
+        $admin->setCity('Soissons');  
+        $manager->persist($admin);  
+
+        $faker = Faker\Factory::create('fr_FR');
+        for($usr = 1; $usr <= 5; $usr++){
+            $user = new User();
+                $user->setEmail($faker->email);
+                $user->setRoles(['PARTNER']);
+                $user->setPassword(
+            $this->passwordEncoder->hashPassword($user, 'secret')
+        );
+            $user->setName($faker->lastName);
+            $user->setAddress($faker->streetAddress);
+        $user->setZipcode(str_replace(' ', '', $faker->postcode));
+        $user->setCity($faker->city);  
+        $manager->persist($user);  
+    }
+
+    for($usr = 1; $usr <= 5; $usr++){
+            $user = new User();
+                $user->setEmail($faker->email);
+                $user->setRoles(['SPORTHALL']);
+                $user->setPassword(
+            $this->passwordEncoder->hashPassword($user, 'secret')
+        );
+            $user->setName($faker->lastName);
+            $user->setAddress($faker->streetAddress);
+        $user->setZipcode(str_replace(' ', '', $faker->postcode));
+        $user->setCity($faker->city);  
+        $manager->persist($user);  
+    }
+        $manager->flush();
+    }
+}
