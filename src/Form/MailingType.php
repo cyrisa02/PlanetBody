e8bb8b850ff4d28da1b2faada5c5409dc6a1f63f
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-08-25 16:42:05 +0200

    Fix Mailing

--- src/Form/MailingType.php
+++ src/Form/MailingType.php
@@ -3,21 +3,33 @@
 namespace App\Form;
 
 use App\Entity\Mailing;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MailingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlenght' => '2',
+                    'maxlenght' => '190',
+                ],
+                'label' => 'Titre du message',
+                'label_attr' => [
+                    'class' => 'form-label  mt-4'
+                ],
+                
+            ])
             ->add('categories')
-            ->add('sporthalls')
-            ->add('partners')
-        ;
+            //->add('sporthalls')
+            ->add('partners');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
